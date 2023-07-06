410a715c6f06e5e029e152dbe242d580c15939a7
mrchndfyzaa <kitagawajoy@gmail.com>
2023-07-07 04:19:46 +0800

    add a solution (new sql solution)

---
+++ revisingTheSelectQuery.sql
@@ -0,0 +1,17 @@
+SELECT * FROM CITY 
+    WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA';
+
+-- Permasalahan
+-- Query all columns for all American cities in the CITY 
+-- table with populations larger than 100000. 
+-- The CountryCode for America is USA.
+
+-- The CITY table is described as follows:
+--========================================
+--      CITY TABLE
+-- Field        Type
+-- ID           NUMBER
+-- NAME         VARCHAR(17)
+-- COUNTRYCODE  VARCHAR(3)
+-- DISTRICT     VARCHAR(20)
+-- POPULATION   NUMBER
